a3269f6c940aa6a7f9c82f6024545baf018b9c6d
Ryan McDonald <rmcdonald595@gmail.com>
2017-06-21 21:07:35 -0700

    Initial Commit

--- DimmingPresentationController.swift
+++ DimmingPresentationController.swift
@@ -9,9 +9,9 @@
 import UIKit
 
 class DimmingPresentationController: UIPresentationController {
-
+ 
     /// Allows presenting viewController to be visible when presenting a viewController with transparent background
     override var shouldRemovePresentersView: Bool {
         return false
     }
-} 
+}
